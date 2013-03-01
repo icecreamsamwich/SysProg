afa57b6e73de345d5b317b7de6b980d59323049c
icecreamsamwich <somhairle.olaoire@mycit.ie>
2013-03-01 12:38:55 +0000

    Hello World test

---
+++ Assignment2/Task4/Parseps.c
@@ -0,0 +1,17 @@
+/*
+ * Parseps.c
+ *
+ *  Created on: Mar 1, 2013
+ *      Author: samoleary
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+	return 0;
+}
